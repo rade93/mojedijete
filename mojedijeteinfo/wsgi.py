deb73e0f957f0c7fc107027edc814aeda87ae759
rade93 <gojkovic007@gmail.com>
2019-11-27 07:12:37 +0100

    Daj Boze da ovo radi

--- mojedijeteinfo/wsgi.py
+++ mojedijeteinfo/wsgi.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import ap, get_wsgi_applicationt
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mojedijeteinfo.settings')
 
