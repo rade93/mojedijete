9593c21fa4520cbefefaf1ef808aa3e43ba04afc
rade93 <gojkovic007@gmail.com>
2019-11-27 07:18:24 +0100

    Upisivanje modula mojedijeteinfo u wsgi fajl

--- mojedijeteinfo/wsgi.py
+++ mojedijeteinfo/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 # from django.core.wsgi import get_wsgi_application
-from django.core.wsgi import ap, get_wsgi_applicationt
+from mojedijeteinfo.wsgi import ap, get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mojedijeteinfo.settings')
 
