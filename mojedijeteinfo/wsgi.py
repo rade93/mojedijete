1f927b78287062eb9f6e5fa6f9f4ef1affd47f7d
rade93 <gojkovic007@gmail.com>
2019-11-27 06:30:08 +0100

    Dokle vise

--- mojedijeteinfo/wsgi.py
+++ mojedijeteinfo/wsgi.py
@@ -7,11 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-from django.contrib.staticfiles.handlers import StaticFilesHandler
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mojedijeteinfo.settings')
 
-application = StaticFilesHandler(get_wsgi_application())
+application = get_wsgi_application()
